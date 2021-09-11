51c06829f83cf44eaec3815e47569407b6fc72a1
soumyanayak98 <soumya.nayak@kreeti.com>
2021-09-11 16:54:52 +0530

    clean ups with partials

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+const webpack = require("webpack");
+environment.plugins.append(
+	"Provide",
+	new webpack.ProvidePlugin({
+		$: "jquery",
+		jquery: "jquery",
+		Popper: ["popper.js", "default"],
+	})
+);
+
+module.exports = environment;
