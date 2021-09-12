d6ccf59d2d14566174e174292deb005fa034ac3e
soumyanayak98 <soumya.nayak@kreeti.com>
2021-09-13 00:18:19 +0530

    flash msgs fixed

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,12 +1,12 @@
 class StocksController < ApplicationController
-  before_action :check_String
+  before_action :check_String, only: [:search]
 
   def search
     @stock = Stock.new_lookup(params[:stock].upcase)
     if @stock.nil?
       respond_to do |format|
         flash.now[:alert] = "Please enter a valid Stock symbol to search"
-        format.js {render partial: 'users/result'}
+        format.js {render partial: 'users/alert'}
       end
     else
       respond_to do |format|
@@ -20,7 +20,7 @@ class StocksController < ApplicationController
     if params[:stock].empty?
       respond_to do |format|
         flash.now[:alert] = 'Please Enter a symbol to search!'
-        format.js {render partial: 'users/result'}
+        format.js {render partial: 'users/alert'}
       end
     end
   end
